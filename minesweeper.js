91dc2fa7fa836922ed3f6e658e4e47370a6fa7c1
Thanh Nhat Tran <94516519+Ginnit08@users.noreply.github.com>
2023-04-22 23:01:26 +0700

    update code

--- minesweeper.js
+++ minesweeper.js
@@ -31,3 +31,41 @@ function saveState() {
     gameState.push(state);
     gameStateIndex++;
   }
+
+  function clearFutureStates() {
+    gameState.splice(gameStateIndex + 1);
+  }
+  
+  function undo() {
+    if (gameStateIndex < 0) {
+      // no more states to undo
+      return;
+    }
+    var state = gameState[gameStateIndex];
+    board.forEach((row, rowIndex) => {
+      row.forEach((tile, colIndex) => {
+        var savedTile = state.board[rowIndex][colIndex];
+        tile.innerText = savedTile.text;
+        tile.className = savedTile.classes.join(' ');
+      });
+    });
+    tilesClicked = state.tilesClicked;
+    gameOver = state.gameOver;
+    clearFutureStates();
+    gameStateIndex--;
+    
+  }
+
+function setMines() {
+    let minesLeft = minesCount;
+    while (minesLeft > 0) { 
+        let r = Math.floor(Math.random() * rows);
+        let c = Math.floor(Math.random() * columns);
+        let id = r.toString() + "-" + c.toString();
+
+        if (!minesLocation.includes(id)) {
+            minesLocation.push(id);
+            minesLeft -= 1;
+        }
+    }
+}
