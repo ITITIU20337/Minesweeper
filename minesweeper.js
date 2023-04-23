9f5c35bf5fda7ab71e5f565257702d2e9ebbc1e7
Thanh Nhat Tran <94516519+Ginnit08@users.noreply.github.com>
2023-04-23 23:12:53 +0700

    update

--- minesweeper.js
+++ minesweeper.js
@@ -149,3 +149,111 @@ function clickTile() {
   saveState()
   }
 }
+
+function revealMines() {
+  for (let r= 0; r < rows; r++) {
+      for (let c = 0; c < columns; c++) {
+          let tile = board[r][c];
+          if (minesLocation.includes(tile.id)) {
+              tile.innerText = bomb;
+              // tile.style.backgroundColor = "red";                
+          }
+      }
+  }
+ 
+ 
+}
+
+function checkMine(r, c) {
+  if (r < 0 || r >= rows || c < 0 || c >= columns) {
+      return;
+  }
+  if (board[r][c].classList.contains("tile-clicked")) {
+      return;
+  }
+
+  board[r][c].classList.add("tile-clicked");
+  tilesClicked += 1;
+
+  let minesFound = 0;
+
+  //top 3
+  minesFound += checkTile(r-1, c-1);      //top left
+  minesFound += checkTile(r-1, c);        //top 
+  minesFound += checkTile(r-1, c+1);      //top right
+
+  //left and right
+  minesFound += checkTile(r, c-1);        //left
+  minesFound += checkTile(r, c+1);        //right
+
+  //bottom 3
+  minesFound += checkTile(r+1, c-1);      //bottom left
+  minesFound += checkTile(r+1, c);        //bottom 
+  minesFound += checkTile(r+1, c+1);      //bottom right
+
+  if (minesFound > 0) {
+      board[r][c].innerText = minesFound;
+      board[r][c].classList.add("x" + minesFound.toString());
+  }
+  else {
+      //top 3
+      checkMine(r-1, c-1);    //top left
+      checkMine(r-1, c);      //top
+      checkMine(r-1, c+1);    //top right
+
+      //left and right
+      checkMine(r, c-1);      //left
+      checkMine(r, c+1);      //right
+
+      //bottom 3
+      checkMine(r+1, c-1);    //bottom left
+      checkMine(r+1, c);      //bottom
+      checkMine(r+1, c+1);    //bottom right
+  }
+
+  if (tilesClicked == rows * columns - minesCount) {
+      document.getElementById("mines-count").innerText = "Cleared";
+      gameOver = true;
+  }
+
+}
+
+function setBomb1(){
+  alert("bomb changed")
+  return bomb="💣"
+  
+}
+function setBomb2(){
+  alert("bomb changed")
+  return bomb="💥"
+}
+function setBomb3(){
+  alert("bomb changed")
+  return bomb="🧨"
+}
+
+function setflag1(){
+   flag='🚩'
+   document.getElementById("flag-button").innerText = flag;
+   return
+}
+function setflag2(){
+   flag='🏳️'
+   document.getElementById("flag-button").innerText = flag;
+   return
+}
+function setflag3(){
+   flag='🏴'
+   document.getElementById("flag-button").innerText = flag;
+   return
+}
+
+function checkTile(r, c) {
+  if (r < 0 || r >= rows || c < 0 || c >= columns) {
+      return 0;
+  }
+  if (minesLocation.includes(r.toString() + "-" + c.toString())) {
+      return 1;
+  }
+  return 0;
+}
